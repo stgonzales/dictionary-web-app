e6c16d2323da7415db2100602e63946b78fc6e57
Stephen Goncalves <stiefo.o@gmail.com>
2023-05-31 18:50:02 +0100

    fix theme 'n layout shift

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,9 +5,11 @@ export const Layout: ParentComponent = ({ children }) => {
   const [state] = useTheme()
 
   return (
-    <div class={`${state.theme} absolute pt-6 md:pt-14 max-w-[366px] md:max-w-[689px] lg:max-w-[737px] mx-auto left-6 right-6`}>
+    <div class={`${state.theme} absolute bottom-0 top-0 right-0 left-0`}>
       <div class="bg-neutral-100 dark:bg-neutral-800 w-full min-h-screen">
-        { children }
+        <div class='mx-auto pt-6 px-6 md:pt-14 max-w-[366px] md:max-w-[689px] lg:max-w-[737px]'>
+          { children }
+        </div>
       </div>
     </div>
   )
